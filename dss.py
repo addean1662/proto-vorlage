bd7c493fe0c635d818d773b287562d1f55478672
addean1662 <addean79@gmail.com>
2025-11-01 20:19:58 -0700

    Update dss.py

--- dss.py
+++ dss.py
@@ -1,1 +1,7 @@
 # Dead Sea Scrolls agent logic
+def get_dss_text(verse):
+    data = {
+        "Isaiah 7:14": "העלמה הרה וילדת בן",  # Fragmentary but nearly identical
+        "Psalm 22:16": "כארו ידי ורגלי"       # Some DSS mss may read this
+    }
+    return data.get(verse, "[Verse not available]")
